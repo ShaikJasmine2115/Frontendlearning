2ead596a4b1a199250a440646effb490998fd930
Shaik Jasmine (Annalect) <jasmine.shaik@annalect.com>
2024-12-24 21:03:26 +0530

    layout changes

--- ums_signup_task/layout.js
+++ ums_signup_task/layout.js
@@ -5,9 +5,6 @@
 //     const password = document.getElementById('register-password').value;
 //     registerUser(username, email, password);
 // });
-// function closeSidebar() {
-//     document.querySelector('.container').style.gridTemplateColumns = '1fr 1fr'; // Set both columns to 1fr when sidebar is closed
-// }
 function closeSidebar() {
     document.querySelector('.container').style.gridTemplateColumns = '1fr 1fr';  // Adjust grid layout
     document.querySelector('.sidebar').style.display = 'none';  // Hide the sidebar
