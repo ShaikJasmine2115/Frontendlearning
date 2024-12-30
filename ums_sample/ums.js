728a71cf1178387c263ce7cb8102004d0e940d36
Shaik Jasmine (Annalect) <jasmine.shaik@annalect.com>
2024-12-30 16:04:02 +0530

    user profile updates

--- ums_sample/ums.js
+++ ums_sample/ums.js
@@ -1,11 +1,13 @@
-document.getElementById('login-toggle').addEventListener('click', function() {
+document.addEventListener("DOMContentLoaded",()=>{
     document.getElementById('login-form').classList.add('active');
     document.getElementById('register-form').classList.remove('active');
-});
+})
 
 document.getElementById('register-toggle').addEventListener('click', function() {
     document.getElementById('register-form').classList.add('active');
     document.getElementById('login-form').classList.remove('active');
+    document.getElementById('register-toggle').style.display = 'none';
+    document.getElementById('login-link').style.display='block';
 });
 
 document.getElementById('login-form').addEventListener('submit', function(event) {
@@ -23,7 +25,12 @@ document.getElementById('register-form').addEventListener('submit', function(eve
     const password = document.getElementById('register-password').value;
     registerUser(userfirstname,userlastname, email, password);
 });
-
+function loginlink(){
+    document.getElementById('login-form').classList.add('active');
+    document.getElementById('register-form').classList.remove('active');
+    document.getElementById('login-link').style.display='none';
+    document.getElementById('register-toggle').style.display = 'block';
+}
 function authenticateUser(username, password) {
     const errorMessage = document.getElementById('error-message');
     fetch('https://dummyjson.com/users')
@@ -38,13 +45,13 @@ function authenticateUser(username, password) {
             } else {
                 document.getElementById('login').style.display = 'none';
                 errorMessage.innerHTML = `<p>Wrong username or password.</p>
-                <button onclick="resetForm()" style="display: block; margin: 10px auto;">Try Again</button>`;
+                <button onclick="resetloginForm()" style="display: block; margin: 10px auto;">Try Again</button>`;
             }
         })
          .catch(error => console.error('Error:', error));
 }
 
-function resetForm() {
+function resetloginForm() {
     document.getElementById('login').style.display = 'block';
      document.getElementById('login-form').reset();
      document.getElementById('error-message').innerHTML = '';
@@ -56,7 +63,7 @@ function registerUser(userfirstname,userlastname, email, password) {
     if (!emailPattern.test(email)) {
       document.getElementById('register').style.display = 'none';
       errorMessage.innerHTML = `<p>Please enter a valid email address.</p>
-                <button id="tryagain" onclick="resetForm()" style="display: block; margin: 10px auto;">Try Again</button>`;
+                <button id="tryagain" onclick="resetregForm()" style="display: block; margin: 10px auto;">Try Again</button>`;
     }
     else{
         sessionStorage.setItem("loggedIn", "true");
@@ -66,4 +73,9 @@ function registerUser(userfirstname,userlastname, email, password) {
         sessionStorage.setItem("password", password);          
         window.location.href = "postlogin.html";
     }
+}
+function resetregForm() {
+    document.getElementById('register').style.display = 'block';
+     document.getElementById('register-form').reset();
+     document.getElementById('error-message').innerHTML = '';
 }
